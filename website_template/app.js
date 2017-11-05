b69eaddd3d9770f7977342e84fb0afd82d6350c9
Patrick Kim <patrick.kim408@gmail.com>
2017-11-05 15:20:39 -0800

    finished 0,1,2,3

--- website_template/app.js
+++ website_template/app.js
@@ -1,5 +1,44 @@
-const message = prompt('Enter a message')
+const message = parseInt(prompt('Choose an option: \n 1: Show messages \n 2: Add a message \n 3: Delete a message \n 0: Quit'));
+
+const messages = [
+    'message 01',
+    'message 02',
+    'message 03',
+    'message 04',
+    'message 05',
+];
 
 // alert('Your input is: ' + message)
-const output = document.getElementById("output")
-output.innerHTML = 'Your message is ' + message
+const output = document.getElementById("output");
+if (message == 1){
+    output.innerHTML = "The current messages are: <br>";
+    for (var i = 0; i < messages.length; i++){
+        output.innerHTML  = output.innerHTML + (i+1) + ": " + messages[i] + "<br>";
+    }
+}
+if (message == 2){
+    var prompt2 = prompt("Enter a new message");
+    messages.push(prompt2);
+    output.innerHTML = "The current messages are: <br>";
+    for (var i = 0; i < messages.length; i++){
+        output.innerHTML = output.innerHTML +  (i+1) + ": " + messages[i] + "<br>";
+    }
+}
+
+if (message == 3){
+    var deleteprompt = prompt("Enter the message index (between 1 and " + messages.length + ")");
+    messages.splice(deleteprompt-1, 1);
+    console.log(messages);
+    output.innerHTML = "The current messages are: <br>";
+    for (var i = 0; i<messages.length; i++){
+        output.innerHTML = output.innerHTML + (i+1) + ": " + messages[i] + "<br>";
+    }
+}
+
+if (message == 0){
+    output.innerHTML = "Good Bye";
+}
+
+else{
+    output.innerHTML = "Inalid input. Refresh and try again";
+}
